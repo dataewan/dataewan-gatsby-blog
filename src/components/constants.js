add07aa0c0db00446cc53d0e2666c8f879ca6322
Ewan Nicolson <ewannic@gmail.com>
2017-11-23 15:13:24 +0000

    adding some colours

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,28 @@
 const Constants = {
   leftwidth: "55%",
   rightwidth: "30%",
+
   smallscale: -0.5,
+
+  grey: '#50514F',
+  darkgrey: '#3c3d3b',
+  lightgrey: '#646563',
+
+  red: '#F25F5C',
+  darkred: '#de4b48',
+  lightred: '#ff7370',
+
+  yellow: '#FFE066',
+  darkyellow: '#ebcc52',
+  lightyellow: '#fff47a',
+
+  blue: '#247BA0',
+  darkblue: '#10678c',
+  lightblue: '#388fb4',
+
+  green: '#70C1B3',
+  darkgreen: '#5cad9f',
+  lightgreen: '#84d5c7'
 }
 
 
