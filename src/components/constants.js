b5b375d8820950fce4308c109c6321f2fcb6f930
Ewan Nicolson <ewannic@gmail.com>
2017-12-02 18:49:20 +0000

    update the link component

--- src/components/constants.js
+++ src/components/constants.js
@@ -7,6 +7,7 @@ const Constants = {
   grey: '#50514F',
   darkgrey: '#3c3d3b',
   lightgrey: '#646563',
+  vlightgrey: '#cccdcb',
 
   red: '#F25F5C',
   darkred: '#de4b48',
@@ -19,6 +20,7 @@ const Constants = {
   blue: '#247BA0',
   darkblue: '#10678c',
   lightblue: '#388fb4',
+  vlightblue: '#b1d7e7',
 
   green: '#70C1B3',
   darkgreen: '#5cad9f',
--- src/components/linknode.js
+++ src/components/linknode.js
@@ -2,19 +2,34 @@ import React, { Component} from 'react';
 import Link from 'gatsby-link'
 
 import Constants from './constants'
+import { rhythm } from '../utils/typography'
 
 class Linknode extends Component {
 
   render() {
+    const prettyname = this.props.to.split('/').slice(-1)[0]
     return (
-      <Link 
-        css={{
-          display: `block`,
-          color: Constants.darkblue,
-        }}
-        to={this.props.to}>
-        {this.props.desc}
-      </Link>
+      <div css={{
+        border: `${Constants.vlightgrey} 1px solid`,
+        marginBottom: rhythm(0.5),
+        width: Constants.leftwidth
+
+      }}>
+        <Link 
+          css={{
+            display: `block`,
+            color: Constants.darkblue,
+            backgroundColor: Constants.vlightblue
+          }}
+          to={this.props.to}>
+          {prettyname}
+        </Link>
+        <div css={{
+          paddingLeft: rhythm(1)
+        }}>
+          {this.props.desc}
+        </div>
+      </div>
     );
   }
 }
