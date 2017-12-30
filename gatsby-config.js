ef0c3a9b789015d39168b373a0433242b5583b57
Ewan Nicolson <ewannic@gmail.com>
2017-12-30 19:05:28 +0000

    adding google analytics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,13 @@ module.exports = {
       }
 
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-39372029-1",
+        // Setting this parameter is optional
+        anonymize: true,
+      },
+    },
   ],
 }
