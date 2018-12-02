449c362d8ac7743fc0c550323f3b820b9eafef02
Ewan Nicolson <ewannic@gmail.com>
2018-12-02 12:06:57 +0000

    writing about MCMC

--- src/pages/learning/statistics/bayesian-inference/index.js
+++ src/pages/learning/statistics/bayesian-inference/index.js
@@ -11,7 +11,8 @@ import {
   Linknode,
   Linktree,
   Constants,
-  Layout
+  Layout,
+  Equation
 } from "../../../../components";
 
 import PartsOfEquation from "./partsOfEquation";
@@ -34,10 +35,37 @@ const StatsIndexPage = () => (
         <PartsOfEquation />
       </Paragraph>
       <Paragraph>
-        Bayes rule gives you a way to get from <i>p(data | θ)</i> to <i>p(θ | data)</i>,
-        flipping round the equation to give you the posterior.
-        An alternative way of thinking about it is that it gives you a way to update your beliefs when new data comes in.
+        Bayes rule gives you a way to get from <i>p(data | θ)</i> to{" "}
+        <i>p(θ | data)</i>, flipping round the equation to give you the
+        posterior. An alternative way of thinking about it is that it gives you
+        a way to update your beliefs when new data comes in.
       </Paragraph>
+      <Paragraph>
+        We go to a lot of hard work to calculate the posterior distribution,
+        which is what the data tells you about the parameters of your model.
+        From the posterior you can make summaries of your analysis to state the
+        likely values of the parameters of the model, and you can also make
+        predictions from the posterior. It is pretty cool!
+      </Paragraph>
+      <Subsection name="Sampling to get the posterior">
+        <Paragraph>
+          The posterior is really hard to calculate exactly, because the
+          evidence term is usually a horrible integral. We take advantage of
+          something we found in{" "}
+          <Link to="./learning/statistics/bayesian-inference/evidence/">
+            the section about evidence
+          </Link>{" "}
+          to rewrite Bayes rule as a similarity relation.
+        </Paragraph>
+        <Paragraph>
+          <Equation equation="$p(\theta | data) = \frac{p(data | \theta) p(\theta)}{p(data)}$" />
+          <Equation equation="$p(\theta | data) \propto p(data | \theta) p(\theta)$" />
+        </Paragraph>
+        <Paragraph>
+          This trick lets us use a family of techniques called <Link to="./learning/statistics/bayesian-inference/mcmc/">Markov Chain Monte Carlo sampling</Link>.
+          These techniques let us get a very good approximation of the posterior distribution, and allows us to solve real world problems.
+        </Paragraph>
+      </Subsection>
     </Section>
   </Layout>
 );
